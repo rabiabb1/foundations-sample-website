4b90352e9f237543f663e7b9fdd418f2822f61e7
rabiabb1 <rabiabbas471@gmail.om>
2021-03-13 21:35:53 +0100

    I changed a lot

--- week1/website.py
+++ week1/website.py
@@ -10,15 +10,17 @@ def index():
 
 @app.route('/first-page')
 def first_page():
-    return render_template('first-page.html', page_title="First Template")
+    return render_template('first-page.html', page_title="About CODE")
 
 
 @app.route('/second-page')
 def second_page():
-    return render_template('second-page.html', page_title="Second Template")
+    return render_template('second-page.html', page_title="My Experience at CODE")
 
-# add additonal pages here using a similar format as above
 
+@app.route('/contact-page')
+def contact_page():
+    return render_template('contact-page.html', page_title="Contact Me")
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
