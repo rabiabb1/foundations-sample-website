bd0b5b8133cbef30aa0531e005beb1002e0e82bf
rabiabb1 <rabiabbas471@gmail.om>
2021-03-31 11:15:59 +0200

    testing deployoment of color_check

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 
-#from week1.website import app # noqa: F401
+from week1.website import app # noqa: F401
 
-from covid_app.website import app  # noqa: F401
+#from covid_app.website import app  # noqa: F401
