999347e21587f5a730da3b076b4d79bda95f3dab
rabiabb1 <rabiabbas471@gmail.om>
2021-03-31 11:26:31 +0200

    re-testing week1

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
-#from week1.website import app # noqa: F401
+from week1.website import app # noqa: F401
 
-from color_check.website import app #noqa: F401 
-
+#from color_check.website import app #noqa: F401 
 
 #from covid_app.website import app  # noqa: F401
